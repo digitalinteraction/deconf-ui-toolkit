0a69221757554dc2f8a254a46a2f21a2fc6b0c26
Rob Anderson <rob.anderson@ncl.ac.uk>
2021-08-19 19:04:21 +0100

    fix prod builds

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -12,26 +12,23 @@ import replace from '@rollup/plugin-replace';
 import babel from '@rollup/plugin-babel';
 // import { terser } from 'rollup-plugin-terser';
 import minimist from 'minimist';
-// import scss from 'rollup-plugin-scss';
 import extractSass from './sass-plugin';
-// import bundleScss from 'rollup-plugin-bundle-scss';
-
-const projectRoot = path.resolve(__dirname, '..');
-
-function readSync(filename) {
-  return fs.readFileSync(path.join(projectRoot, filename), 'utf8');
-}
 
 // Get browserslist config and remove ie from es build targets
-const esbrowserslist = JSON.parse(
-  fs.readFileSync('./package.json', 'utf8')
-).browserslist.filter(e => e.substring(0, 2) !== 'ie');
+const esbrowserslist = fs
+  .readFileSync('./.browserslistrc')
+  .toString()
+  .split('\n')
+  .filter(entry => entry && entry.substring(0, 2) !== 'ie');
+
+// Extract babel preset-env config, to combine with esbrowserslist
+const babelPresetEnvConfig = require('../babel.config').presets.filter(
+  entry => entry[0] === '@babel/preset-env'
+)[0][1];
 
 const argv = minimist(process.argv.slice(2));
 
-const moduleName = 'DeconfUI';
-const fileName = 'dist/deconf-ui';
-const sorucemap = true;
+const projectRoot = path.resolve(__dirname, '..');
 
 const baseConfig = {
   input: 'src/module.ts',
@@ -43,16 +40,12 @@ const baseConfig = {
             find: '@',
             replacement: `${path.resolve(projectRoot, 'src')}`
           }
-        ],
-        customResolver: resolve({
-          extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'] // .scss here ?
-        })
+        ]
       }),
-      // bundleScss()
       extractSass({
         prependData: [
-          readSync('src/scss/common.scss'),
-          readSync('src/scss/app.scss')
+          fs.readFileSync('./src/scss/common.scss'),
+          fs.readFileSync('./src/scss/app.scss')
         ].join('\n'),
         copyFiles: [
           {
@@ -62,7 +55,6 @@ const baseConfig = {
         ],
         filename: null
       })
-      // scss()
     ],
     replace: {
       preventAssignment: true,
@@ -95,7 +87,12 @@ const baseConfig = {
         }
       }
     },
-    postVue: [],
+    postVue: [
+      resolve({
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue']
+      }),
+      commonjs()
+    ],
     babel: {
       exclude: 'node_modules/**',
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
@@ -104,31 +101,16 @@ const baseConfig = {
   }
 };
 
-// global.myFunction = (...args) => {
-//   console.log('myFunction', ...args);
-// };
-
 // ESM/UMD/IIFE shared settings: externals
 // Refer to https://rollupjs.org/guide/en/#warning-treating-module-as-external-dependency
+// list external dependencies, exactly the way it is written in the import statement.
 const external = [
-  // list external dependencies, exactly the way it is written in the import statement.
-  // eg. 'jquery'
   'vue',
   '@fortawesome/vue-fontawesome',
   'copy-to-clipboard',
   '@openlab/deconf-shared',
   'lodash.debounce'
 ];
-
-// UMD/IIFE shared settings: output.globals
-// Refer to https://rollupjs.org/guide/en#output-globals for details
-const globals = {
-  // Provide global variable names to replace your external imports
-  // eg. jquery: '$'
-  vue: 'Vue',
-  '@fortawesome/vue-fontawesome': 'vue-fontawesome',
-  'copy-to-clipboard': 'copyToClipboard'
-};
 
 // Customize configs for individual targets
 const buildFormats = [];
@@ -137,10 +119,9 @@ if (!argv.format || argv.format === 'es') {
     ...baseConfig,
     external,
     output: {
-      file: `${fileName}.esm.js`,
+      file: `dist/deconf-ui.esm.js`,
       format: 'esm',
-      exports: 'named',
-      sourcemap: sorucemap
+      exports: 'named'
     },
     plugins: [
       replace(baseConfig.plugins.replace),
@@ -149,7 +130,15 @@ if (!argv.format || argv.format === 'es') {
       ...baseConfig.plugins.postVue,
       babel({
         ...baseConfig.plugins.babel,
-        presets: [['@babel/preset-env', { targets: esbrowserslist }]]
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              ...babelPresetEnvConfig,
+              targets: esbrowserslist
+            }
+          ]
+        ]
       }),
       commonjs({
         extensions: ['.js', '.ts', '.vue']
@@ -159,80 +148,5 @@ if (!argv.format || argv.format === 'es') {
   buildFormats.push(esConfig);
 }
 
-if (!argv.format || argv.format === 'cjs') {
-  //
-  // NOTE: "unpkg" has been removed from the package.json
-  // - "main": "dist/deconf-ui.ssr.js",
-  //
-  // const umdConfig = {
-  //   ...baseConfig,
-  //   external,
-  //   output: {
-  //     compact: true,
-  //     file: `${fileName}.ssr.js`,
-  //     format: 'cjs',
-  //     name: moduleName,
-  //     exports: 'auto',
-  //     globals,
-  //     sourcemap: sorucemap
-  //   },
-  //   plugins: [
-  //     replace(baseConfig.plugins.replace),
-  //     ...baseConfig.plugins.preVue,
-  //     vue({
-  //       ...baseConfig.plugins.vue,
-  //       template: {
-  //         ...baseConfig.plugins.vue.template,
-  //         optimizeSSR: true
-  //       }
-  //     }),
-  //     ...baseConfig.plugins.postVue,
-  //     babel(baseConfig.plugins.babel),
-  //     commonjs()
-  //   ]
-  // };
-  // buildFormats.push(umdConfig);
-}
-
-if (!argv.format || argv.format === 'iife') {
-  //
-  // NOTE: "unpkg" has been removed from the package.json
-  // - "unpkg": "dist/deconf-ui.min.js",
-  //
-  // const unpkgConfig = {
-  //   ...baseConfig,
-  //   external,
-  //   output: {
-  //     compact: true,
-  //     file: `${fileName}.min.js`,
-  //     format: 'iife',
-  //     name: moduleName,
-  //     exports: 'auto',
-  //     globals,
-  //     sourcemap: sorucemap
-  //   },
-  //   plugins: [
-  //     replace(baseConfig.plugins.replace),
-  //     ...baseConfig.plugins.preVue,
-  //     vue(baseConfig.plugins.vue),
-  //     ...baseConfig.plugins.postVue,
-  //     babel(baseConfig.plugins.babel),
-  //     commonjs(),
-  //     terser({
-  //       output: {
-  //         ecma: 5
-  //       }
-  //     })
-  //   ]
-  // };
-  // buildFormats.push(unpkgConfig);
-}
-
 // Export config
 export default buildFormats;
-
-//
-//
-//
-
-// https://stackoverflow.com/questions/64438777/trying-to-build-vue-component-library-using-rollup-and-vuejs-3
