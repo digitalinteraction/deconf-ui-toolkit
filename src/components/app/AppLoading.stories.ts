f4cb9530931558eb4e8a38195780cb10df2d5c8c
Rob Anderson <rob.anderson@ncl.ac.uk>
2021-08-20 13:19:20 +0100

    refactor: improve AppLoading story

--- src/components/app/AppLoading.stories.ts
+++ src/components/app/AppLoading.stories.ts
@@ -16,3 +16,10 @@ const Template: Story = (args, { argTypes }) => ({
 export const Default = Template.bind({});
 Default.args = {};
 Default.parameters = { layout: 'fullscreen' };
+
+export const Mobile = Template.bind({});
+Mobile.args = {};
+Mobile.parameters = {
+  layout: 'fullscreen',
+  viewport: { defaultViewport: 'mobile2' }
+};
