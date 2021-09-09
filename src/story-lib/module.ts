fd84d8f8916ba6017e8424583656e51bd678316a
Rob Anderson <robanderson@hey.com>
2021-09-09 15:40:26 +0100

    fix: fix broken export

--- src/story-lib/module.ts
+++ src/story-lib/module.ts
@@ -2,6 +2,5 @@ export * from './components';
 export * from './content';
 export * from './dates';
 export * from './fontawesome';
-export * from './settings';
 export * from './schedule';
 export * from './template';
