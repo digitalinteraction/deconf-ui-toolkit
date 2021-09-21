f92bd009c7a8e2b57840cfd42d70f541f0e668a7
Rob Anderson <robanderson@hey.com>
2021-09-21 12:34:20 +0100

    chore: prep for countdown days

--- src/lib/countdowns.ts
+++ src/lib/countdowns.ts
@@ -4,23 +4,33 @@ export interface CountdownComponents {
   seconds: number;
 }
 
+const MS_PER_SECOND = 60 * 1000;
+const MS_PER_MINUTE = 60 * 60 * 1000;
+const MS_PER_HOUR = 60 * 60 * 60 * 1000;
+const MS_PER_DAY = 24 * 60 * 60 * 60 * 1000;
+
 export function getCountdown(
   currentDate: Date,
   targetDate: Date
 ): CountdownComponents {
   let remaining = targetDate.getTime() - currentDate.getTime();
 
+  // Remaining days
+  // TODO:
+  // const days = Math.floor(remaining / MS_PER_DAY);
+  // remaining = remaining - days * MS_PER_DAY;
+
   // Remaining hours
-  const hours = Math.floor(remaining / 3600000);
-  remaining = remaining - hours * 3600000;
+  const hours = Math.floor(remaining / MS_PER_HOUR);
+  remaining = remaining - hours * MS_PER_HOUR;
 
   // Remaining minutes
-  const minutes = Math.floor(remaining / 60000);
-  remaining = remaining - minutes * 60000;
+  const minutes = Math.floor(remaining / MS_PER_MINUTE);
+  remaining = remaining - minutes * MS_PER_MINUTE;
 
   // Remaining seconds
-  const seconds = Math.floor(remaining / 1000);
-  remaining = remaining - seconds * 1000;
+  const seconds = Math.floor(remaining / MS_PER_SECOND);
+  remaining = remaining - seconds * MS_PER_SECOND;
 
   return { hours, minutes, seconds };
 }
@@ -29,7 +39,7 @@ export function getCountdownMessage(
   { hours, minutes, seconds }: CountdownComponents,
   tc: (key: string, value: number) => string,
   seperator = ' '
-) {
+): string {
   const convert = (value: number, unit: string) => {
     if (!value) return null;
     return `${value} ${tc(`deconf.general.${unit}`, value)}`;
