cf732c95ea7a40e33d531188683443e820c4a24a
Rob Anderson <robanderson@hey.com>
2021-11-17 13:45:03 +0000

    fix: add missing session module exports

--- src/session/module.ts
+++ src/session/module.ts
@@ -10,8 +10,8 @@ export { default as IframeEmbed } from './IframeEmbed.vue';
 export { default as InterestSection } from './InterestSection.vue';
 export { default as LanguageWarning } from './LanguageWarning.vue';
 export { default as SecondaryEmbed } from './SecondaryEmbed.vue';
+export { default as SessionLayout } from './SessionLayout.vue';
 export { default as SessionLink } from './SessionLink.vue';
 export { default as SessionState } from './SessionState.vue';
+export { default as SessionView } from './SessionView.vue';
 export { default as SidebarItem } from './SidebarItem.vue';
-
-// TODO: revise
