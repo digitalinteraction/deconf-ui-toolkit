db1d42594105ffbbd98a2d840c4e2a6ecb05b01d
Rob Anderson <robanderson@hey.com>
2021-11-17 17:53:30 +0000

    chore: improve deprecation code

--- src/session/module.ts
+++ src/session/module.ts
@@ -1,15 +1,11 @@
-import PrimaryEmbed from './PrimaryEmbed.vue';
-
-/** @deprecated renamed to `PrimaryEmbed` */
-const SessionEmbed = PrimaryEmbed;
-export { PrimaryEmbed, SessionEmbed };
-
 export { default as AttendanceSection } from './AttendanceSection.vue';
 export { default as Countdown } from './Countdown.vue';
 export { default as IframeEmbed } from './IframeEmbed.vue';
 export { default as InterestSection } from './InterestSection.vue';
 export { default as LanguageWarning } from './LanguageWarning.vue';
+export { default as PrimaryEmbed } from './PrimaryEmbed.vue';
 export { default as SecondaryEmbed } from './SecondaryEmbed.vue';
+export { SessionEmbed } from './session-embed';
 export { default as SessionLayout } from './SessionLayout.vue';
 export { default as SessionLink } from './SessionLink.vue';
 export { default as SessionState } from './SessionState.vue';
---
+++ src/session/session-embed.ts
@@ -0,0 +1,6 @@
+import { default as PrimaryEmbed } from './PrimaryEmbed.vue';
+
+/** @deprecated renamed to `PrimaryEmbed` */
+const SessionEmbed = PrimaryEmbed;
+
+export { SessionEmbed };
